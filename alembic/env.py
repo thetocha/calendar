0c10dc47fd300ab8f33e6f7557661f014750ed2b
Anton Hubko <antongub3@gmail.com>
2023-11-16 11:06:07 +0300

    [all models] feature models

--- alembic/env.py
+++ alembic/env.py
@@ -5,13 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.database import Base
-import config as conf
+from config import DbSettings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option('sqlalchemy.url', conf.SQLALCHEMY_DATABASE_URL)
+config.set_main_option('sqlalchemy.url', DbSettings().db_url)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/users/models.py
+++ app/users/models.py
@@ -1,10 +1,33 @@
 import uuid
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, ENUM
+from enum import Enum
 
 from app.database import Base
+
+
+class CourseEnum(Enum):
+    first = "First"
+    second = "Second"
+    third = "Third"
+    forth = "Forth"
+
+
+class WeekDayEnum(Enum):
+    monday = "Monday"
+    tuesday = "Tuesday"
+    wednesday = "Wednesday"
+    thursday = "Thursday"
+    friday = "Friday"
+    saturday = "Saturday"
+    sunday = "Sunday"
+
+
+class WeekEnum(Enum):
+    odd = "Odd"
+    even = "Even"
 
 
 class User(Base):
@@ -14,12 +37,13 @@ class User(Base):
     first_name = Column(String(100))
     last_name = Column(String(100), nullable=False)
     username = Column(String(100), nullable=False, unique=True)
-    password = Column(String(20), nullable=False)
-    group = Column(Integer, ForeignKey("groups.id"))
-    role = Column(Integer, ForeignKey("roles.id"))
+    password = Column(String(1000), nullable=False)
+    group_role = Column(Integer, ForeignKey("user_group_roles.id"))
+    user_role = Column(Integer, ForeignKey("user_roles.id"))
 
-    roles = relationship("Role", back_populates="users")
-    groups = relationship("Group", back_populates="users")
+    user_roles = relationship("UserRole", back_populates="users")
+    user_group_roles = relationship("UserGroupRole", back_populates="users")
+    notifications = relationship("Notification", back_populates="users")
 
 
 class Role(Base):
@@ -28,13 +52,103 @@ class Role(Base):
     id = Column(Integer, primary_key=True, index=True)
     role_name = Column(String(20), nullable=False)
 
-    users = relationship("User", back_populates="roles")
+    user_roles = relationship("User", back_populates="roles")
+
+
+class UserRole(Base):
+    __tablename__ = "user_roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(UUID, ForeignKey("users.id"))
+    role_id = Column(Integer, ForeignKey("roles.id"))
+
+    users = relationship("User", back_populates="user_roles")
+    roles = relationship("Role", back_populates="user_roles")
+
+
+class GroupRole(Base):
+    __tablename__ = "group_roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    role_name = Column(String(100), nullable=False)
+
+    user_group_roles = relationship("UserGroupRole", back_populates="group_roles")
+
+
+class UserGroupRole(Base):
+    __tablename__ = "user_group_roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(UUID, ForeignKey("users.id"))
+    group_id = Column(UUID, ForeignKey("groups.id"))
+    role_id = Column(Integer, ForeignKey("group_roles.id"))
+
+    groups = relationship("Group", back_populates="group_roles")
+    group_roles = relationship("GroupRole", back_populates="user_group_roles")
+    users = relationship("User", back_populates="user_group_roles")
 
 
 class Group(Base):
     __tablename__ = "groups"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), index=True)
     number = Column(Integer, nullable=False)
+    course = Column(ENUM(CourseEnum), nullable=False)
 
-    users = relationship("User", back_populates="groups")
+    group_roles = relationship("UserGroupRole", back_populates="groups")
+    group_events = relationship("EventGroup", back_populates="groups")
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), index=True)
+    professor = Column(String(30))
+    place = Column(String(20))
+    week = Column(ENUM(WeekEnum), nullable=False)
+    weekday = Column(ENUM(WeekDayEnum), nullable=False)
+
+    event_groups = relationship("EventGroup", back_populates="events")
+
+
+class Attendance(Base):
+    __tablename__ = "attendance"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), index=True)
+    user = Column(UUID, ForeignKey("users.id"))
+    event = Column(UUID, ForeignKey("events.id"))
+    attended = Column(Boolean)
+    promised = Column(Boolean)
+
+    users = relationship("User", back_populates="attendance")
+    events = relationship("Event", back_populates="attendance")
+
+
+class EventGroup(Base):
+    __tablename__ = "event_groups"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), index=True)
+    group = Column(UUID, ForeignKey("groups.id"))
+    event = Column(UUID, ForeignKey("events.id"))
+
+    groups = relationship("Group", back_populates="group_events")
+    events = relationship("Event", back_populates="event_groups")
+
+
+class StatusEnum(Enum):
+    sent = "Sent"
+    not_sent = "Not Sent"
+    error = "Error"
+    sending = "Sending"
+
+
+class Notification(Base):
+    __tablename__ = "notifications"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), index=True)
+    text = Column(String(1000), nullable=False)
+    creation_date = Column(DateTime, nullable=False)
+    status = Column(ENUM(StatusEnum), nullable=False)
+    user = Column(UUID, ForeignKey("users.id"))
+
+    users = relationship("User", back_populates="notifications")
